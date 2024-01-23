eb188e8849e2b255848c60cdeab152978f6ff6e9
Artyom_PC <lilapank@yandex.ru>
2024-01-23 03:30:00 +0300

    fix: check on empty date in validate

--- internal/handler/validateData.go
+++ internal/handler/validateData.go
@@ -8,11 +8,15 @@ import (
 
 func validateData(person models.Person) error {
 	data := fmt.Sprintf("%s%s%s", person.Name, person.Surname, person.Patronymic)
-	
+
+	if len(data) == 0 {
+		return models.ErrInvalidData
+	}
+
 	data = strings.ToLower(data)
 	for _, v := range data {
 		if v < 'a' || v > 'z' {
-			return fmt.Errorf("invalid data")
+			return models.ErrInvalidData
 		}
 	}
 	return nil
